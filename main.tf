c43b49761a3c126ea27ee6f06d57f48a3f5a85b6
Nurjamal14 <126831769+Nurjamal14@users.noreply.github.com>
2023-06-11 12:19:50 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-region = "us-east-2"
+region = "us-west-1"
 }
 
 
 resource "aws_instance" "web" {
-ami = "ami-092b51d9008adea15"
+ami_id = "ami-0f8e81a3da6e2510a"
 instance_type = "t2.micro"
-availability_zone = "us-east-2a"
-key_name = "mylaptop"
+availability_zone = "us-west-1a"
+key_name = "my-laptop-key"
 }
